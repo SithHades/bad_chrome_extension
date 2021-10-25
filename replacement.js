cb3a3d7406b9da90179afa6a32016c6391c9825e
Luca Knaack <l.knaack93@gmail.com>
2021-10-25 17:32:05 +0200

    Update replacement.js

--- replacement.js
+++ replacement.js
@@ -1,7 +1,7 @@
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a, div')
 
 for(let i = 0; i < text.length; i++) {
-    if(text[i].innerHTML.includes('Hurensohn')) {
-        text[i].innerHTML = text[i].innerHTML.replace('Hurensohn', 'Sohn einer räudigen Straßendirne')
+    if(text[i].innerHTML.includes('Word1')) {
+        text[i].innerHTML = text[i].innerHTML.replace('Word1', 'Word2')
     }
 }
